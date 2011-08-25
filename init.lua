11482f3c562eaf661c9ad107ef421c77dc6aa322
Michael Ebens <michael@nova-fusion.com>
2011-08-25 19:50:27 +1000

    Initial commit.

---
+++ init.lua
@@ -0,0 +1,37 @@
+local path = ({...})[1]:gsub("%.init", "")
+require(path .. ".Tween")
+require(path .. ".AttrTween")
+require(path .. ".ease")
+
+function Entity:animate(duration, t)
+  if not ammo._world then return end
+  
+  local ease = t.ease
+  local onComplete = t.onComplete
+  t.ease = nil
+  t.onComplete = nil
+  
+  local tween = AttrTween:new(self, duration, t, Tween.ONESHOT, onComplete, ease)
+  (self._world or ammo._world):add(tween)
+  return tween:start()
+end
+
+function delay(secs, func)
+  if not ammo._world then return end
+  local t = Tween:new(secs, Tween.ONESHOT, func)
+  ammo._world:add(t)
+  return t:start()
+end
+
+function tween(obj, duration, t)
+  if not ammo._world then return end
+  
+  local ease = t.ease
+  local onComplete = t.onComplete
+  t.ease = nil
+  t.onComplete = nil
+  
+  local t = AttrTween:new(obj, duration, t, Tween.ONESHOT, onComplete, ease)
+  ammo._world:add(t)
+  return t:start()
+end
